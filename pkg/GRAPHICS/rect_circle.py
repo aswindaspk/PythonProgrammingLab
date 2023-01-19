47cfe66976ff3c86f55fc8b378987d8a1943f051
Aswin <89696235+aswindaspk@users.noreply.github.com>
2023-01-19 13:01:39 +0530

    Add files via upload

---
+++ pkg/GRAPHICS/rect_circle.py
@@ -0,0 +1,9 @@
+def rectarea (l,b):
+    print("\nArea of Rectangle: ",l*b)
+def rectperi (l,b):
+    print("\nPerimeter of rectangle: ",2*(l+b))
+    
+def circlearea (r):
+    print("\nArea of Circle: ",3.14*r*r)
+def circleperi (r):
+    print("\nPerimeter of circle: ",2*3.14*r)
